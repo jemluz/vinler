4a157577d07c6399e641c497a8cbc28a14f9dcb3
Jemima <contatojemima@gmail.com>
2019-09-02 08:44:34 -0300

    criação das funções de validação

---
+++ api/validation.js
@@ -0,0 +1,39 @@
+/*
+  BACKEND - FUNÇÕES DE VALIDAÇÃO 
+
+  existsOrError - verifica se o valor recebido existe, se não existe retorna uma mensagem de erro
+    > verifica que o valor está setado
+    > verifica se é um array vazio
+    > verifica se é uma string vazia
+
+  notExistsOrError - verifica se o valor recebido não existe, se existe retorna uma mensagem de erro
+    > testa o existsOrError
+    > se passar no teste, retorna a mensagem de erro
+    > se não passar no teste, retorna ok
+
+  equalsOrError - verifica se dois valores são iguais, se não forem, retorna uma mensagem de erro
+*/
+
+module.exports = app => {
+  function existsOrError(value, msg) {
+    if(!value) throw msg
+    if(Array.isArray(value) && value.length === 0) throw msg
+    if(typeof value === 'string' && !value.trim()) throw msg
+  }
+  
+  function notExistsOrError(value, msg) {
+    try {
+      existsOrError(value, msg)
+    } catch(msg) {
+      return 
+    }
+    throw msg
+  } 
+  
+  function equalsOrError(valueA, valueB, msg) {
+    if(valueA !== valueB) throw msg
+  }
+
+  return { existsOrError, notExistsOrError, equalsOrError }
+}
+
